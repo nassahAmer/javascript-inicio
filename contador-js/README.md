51a5a70f07951ede45d9026fe95637da4c35d3d9
nassahAmer <hassan.amer.rr@gmail.com>
2022-07-05 15:12:00 -0400

    Pequenos ajustes JS e readme

--- contador-js/README.md
+++ contador-js/README.md
@@ -2,6 +2,6 @@
 ## Estudos com javascript
 
 **Estudo de caso:** Criando um contador com javascript
+
 **Plataforma** *DIO - Digital Innovation One*
-<br />
 ### Santander Bootcamp Fullstack Developer
--- contador-js/assets/js/scripts.js
+++ contador-js/assets/js/scripts.js
@@ -3,11 +3,13 @@ var currentNumberWrapper = document.getElementById('currentNumber');
 // Variável com valor que será modificado no innerHTML
 var currentNumber = 0;
 
+// Função incremento
 function increment() {
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
 
+// Função decremento 
 function decrement() {
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
